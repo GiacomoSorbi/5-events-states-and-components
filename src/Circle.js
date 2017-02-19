8cc279cc851c1b7639fa0fef4d916c09ada47dbf
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-19 16:45:09 +0100

    initial project setup

---
+++ src/Circle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Circle extends Component {
+  render() {
+    return (
+        <div className={'circle'}></div>
+    );
+  }
+}
+
+export default Circle;
